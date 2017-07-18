0edbfbc5baa70208505b9e396e0ac728902d8d3a
Vidal Ekechukwu <dev+vekh@hashrocket.com>
2017-07-18 11:42:35 -0500

    Return developer details in the api posts response

--- app/controllers/api/developer_posts_controller.rb
+++ app/controllers/api/developer_posts_controller.rb
@@ -3,14 +3,19 @@ module Api
     def index
       developer = Developer.find_by!(email: params[:email])
 
-      latest_posts =
-        Post
-          .joins(:developer)
-          .where('developers.email': params[:email])
-          .order(created_at: :desc)
-          .limit(3)
+      latest_posts = Post.joins(:developer)
+        .where('developers.email': params[:email])
+        .order(created_at: :desc)
+        .limit(3)
+        .pluck(:title, :slug)
+        .map { |t, s| {title: t, slug: s} }
 
-      data = {'data': latest_posts.pluck(:title, :slug).map { |t, s| {title: t, slug: s} } }
+      data = {
+        data: {
+          developer: developer.slice(:email, :username),
+          posts: latest_posts,
+        }
+      }
 
       respond_to do |format|
         format.json {render json: data.to_json}
--- spec/controllers/api/developer_posts_controller_spec.rb
+++ spec/controllers/api/developer_posts_controller_spec.rb
@@ -1,32 +1,66 @@
 require 'rails_helper'
 
 describe Api::DeveloperPostsController, type: :controller do
-  let!(:developer) { FactoryGirl.create :developer, email: 'liz.lemon@hashrocket.com' }
+  let!(:developer) do
+    FactoryGirl.create(
+      :developer,
+      email: 'liz.lemon@hashrocket.com',
+      username: 'lizlemon',
+    )
+  end
 
   describe '#index' do
     it "shows a Rocketeer's TIL posts" do
       other_developer_post = FactoryGirl.create(:post, created_at: Time.now)
 
-      post1 = FactoryGirl.create(:post, developer: developer, title: 'How I learned to love JavaScript', slug: '123ABC', created_at: Time.now)
-      post3 = FactoryGirl.create(:post, developer: developer, title: 'JQuery Protips', slug: 'xyz000', created_at: Time.now - 2.days)
-      post2 = FactoryGirl.create(:post, developer: developer, title: 'Learn Vimium', slug: 'def678', created_at: Time.now - 1.day)
+      post1 = FactoryGirl.create(
+        :post,
+        created_at: Time.now,
+        developer: developer,
+        slug: '123ABC',
+        title: 'How I learned to love JavaScript',
+      )
+
+      post3 = FactoryGirl.create(
+        :post,
+        created_at: Time.now - 2.days,
+        developer: developer,
+        slug: 'xyz000',
+        title: 'JQuery Protips',
+      )
+
+      post2 = FactoryGirl.create(
+        :post,
+        created_at: Time.now - 1.day,
+        developer: developer,
+        slug: 'def678',
+        title: 'Learn Vimium',
+      )
 
       get :index, params: {email: 'liz.lemon@hashrocket.com'}, format: :json
 
-      expected_body = {data: [
-        {
-          title: 'How I learned to love JavaScript',
-          slug: '123ABC'
-        },
-        {
-          title: 'Learn Vimium',
-          slug: 'def678'
-        },
-        {
-          title: 'JQuery Protips',
-          slug: 'xyz000'
+      expected_body = {
+        data: {
+          developer: {
+            email: 'liz.lemon@hashrocket.com',
+            username: 'lizlemon',
+          },
+          posts: [
+            {
+              title: 'How I learned to love JavaScript',
+              slug: '123ABC'
+            },
+            {
+              title: 'Learn Vimium',
+              slug: 'def678'
+            },
+            {
+              title: 'JQuery Protips',
+              slug: 'xyz000'
+            }
+          ]
         }
-      ]}.to_json
+      }.to_json
 
       expect(response.body).to eq expected_body
       expect(response.status).to eq 200
@@ -35,7 +69,15 @@ describe Api::DeveloperPostsController, type: :controller do
     it 'responds with no data when there are no TILs' do
       get :index, params: {email: 'liz.lemon@hashrocket.com'}, format: :json
 
-      expected_body = {data: []}.to_json
+      expected_body = {
+        data: {
+          developer: {
+            email: 'liz.lemon@hashrocket.com',
+            username: 'lizlemon',
+          },
+          posts: []
+        }
+      }.to_json
 
       expect(response.body).to eq expected_body
       expect(response.status).to eq 200
