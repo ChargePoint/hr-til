086e875ab982119082aad2aa346c9bd3c715b84f
Achal-Aggarwal <theachalaggarwal@gmail.com>
2017-12-19 16:41:07 +0530

    Do not dump schema after migrations. To dump it needs psql bins

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module HrTil
   class Application < Rails::Application
     config.active_record.schema_format = :sql
+    config.active_record.dump_schema_after_migration = false
     config.autoload_paths << Rails.root.join('lib')
     config.time_zone = 'Eastern Time (US & Canada)'
 
